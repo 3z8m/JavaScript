f1ae1b88bf7e41adaf9dff421f7b523cf6807fdc
3z8m <89313312+3z8m@users.noreply.github.com>
2024-12-30 08:44:52 +0900

    Delete routes directory

--- routes/three_fps.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('cg/three_fps', { title: '金属箔工場' });
-});
-
-module.exports = router;
--- routes/three_move.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('cg/three_move', { title: '工場見学' });
-});
-
-module.exports = router;
