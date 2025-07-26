d4b9304107e6791dd9e588e934e79acfd13a8147
3z8m <89313312+3z8m@users.noreply.github.com>
2025-07-27 01:51:55 +0900

    Delete next-mysql directory

--- next-mysql/src/app/(public)/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function TopPage() {
-	return (
-		<div className="container mx-auto px-4 py-8">
-			Hello!
-		</div>
-	)
-}
--- next-mysql/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-// クラス名を結合し、重複を排除するために使用
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
