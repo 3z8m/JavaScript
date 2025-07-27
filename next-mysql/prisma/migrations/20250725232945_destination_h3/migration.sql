0322b5ab846581457a4a7e9dc9e484bf62569273
3z8m <89313312+3z8m@users.noreply.github.com>
2025-07-27 09:47:50 +0900

    Delete next-mysql directory

--- next-mysql/prisma/migrations/20250725232945_destination_h3/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `destination_h` ADD COLUMN `place` VARCHAR(191) NULL,
-    MODIFY `presence` VARCHAR(191) NULL,
-    MODIFY `destination` VARCHAR(191) NULL,
-    MODIFY `lunch` VARCHAR(191) NULL;
