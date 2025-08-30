11a164bf304a2f320a7ed4555a86a34a0fe4d41e
3z8m <89313312+3z8m@users.noreply.github.com>
2025-08-31 01:10:15 +0900

    Add files via upload

---
+++ (public)/chart/page.tsx
@@ -0,0 +1,10 @@
+import { Component } from "@/components/chart/ExampleChart";
+
+export default function Page() {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold mb-4">Example Chart</h1>
+            <Component />
+        </div>
+    );
+}
