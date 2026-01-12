40357ad4fae7a28fcf1101c6cabb308dd4fb0920
3z8m <89313312+3z8m@users.noreply.github.com>
2026-01-12 21:45:32 +0900

    Delete app directory

--- app/(public)/destination_i/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DestinationTable } from "@/components/destination_i/DestinationTable";
-
-export default function DestinationPage() {
-	return (
-		<div className="main flex flex-row gap-1 px-2 py-4 w-full">
-			<DestinationTable />
-		</div>
-	);
-}
