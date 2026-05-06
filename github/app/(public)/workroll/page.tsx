0aaf0f2b4df1dce7cf70f54b7c1b69b78c3d15ca
3z8m <89313312+3z8m@users.noreply.github.com>
2026-05-06 20:05:26 +0900

    Delete github directory

--- github/app/(public)/workroll/page.tsx
+++
@@ -1,9 +0,0 @@
-import { WorkrollTable } from "@/components/workroll/WorkrollTable";
-
-export default function WorkrollPage() {
-	return (
-		<div className="main flex flex-row gap-1 px-2 py-4 w-full">
-			<WorkrollTable />
-		</div>
-	);
-}
