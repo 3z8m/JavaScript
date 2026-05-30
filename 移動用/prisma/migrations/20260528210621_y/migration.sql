6a4693b723e5fbc2e2b0ef23b5381d43c361c045
3z8m <89313312+3z8m@users.noreply.github.com>
2026-05-30 22:26:32 +0900

    Delete 移動用 directory

--- 移動用/prisma/migrations/20260528210621_y/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `diameter_ml` on table `workroll` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `workroll` MODIFY `diameter_ml` DOUBLE NOT NULL;
