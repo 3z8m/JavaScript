e72333d4342f2f10d9879d26b08471335b2f5c77
3z8m <89313312+3z8m@users.noreply.github.com>
2026-06-11 05:43:24 +0900

    Delete 移動用 directory

--- 移動用/app/(public)/imroll/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ImrollTable } from "@/components/imroll/ImrollTable";
-
-export default function ImrollPage() {
-	return (
-		<div className="main flex flex-row gap-1 px-2 py-4 w-full">
-			<ImrollTable />
-		</div>
-	);
-}
